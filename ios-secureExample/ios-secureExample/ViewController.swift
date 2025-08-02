d916325cb0dd7c7b1b428817a22dd105292dc9b1
Prasanth Podalakur <prasanth.ios@gmail.com>
2025-08-02 18:52:43 +0800

    SPM creataed

---
+++ ios-secureExample/ios-secureExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-secureExample
+//
+//  Created by PrasanthPodalakur on 02/08/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios-secureExample/ios-secureExampleTests/ios_secureExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_secureExampleTests.swift
+//  ios-secureExampleTests
+//
+//  Created by PrasanthPodalakur on 02/08/25.
+//
+
+import Testing
+@testable import ios_secureExample
+
+struct ios_secureExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
